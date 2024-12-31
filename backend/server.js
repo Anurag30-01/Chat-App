3ea08e49b0bc8c4067f160be55c2210ba28b5bbc
Anurag Swaroop <anuragswaroop3012004@gmail.com>
2024-12-31 17:17:51 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // const  express = require("express")
 // const dotenv =require("dotenv");
+import path, { dirname } from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,8 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +24,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+});
 
 // app.get("/",(req,res)=>{
 //     // root route http://localhost:5000/
